7c09d791b49c68bc3e852b5da6363f957808cc98
edsonbrunoa <bruno.alves.ba442@gmail.com>
2023-03-02 12:23:06 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const caixasImagem = document.querySelectorAll('.caixa-img');
+
+caixasImagem.forEach(function(caixa){
+    const imagens = caixa.querySelectorAll('img');
+    let idx = 0;
+
+    function carrosel(){
+        idx++;
+        if(idx > imagens.length -1){
+            idx = 0;
+        }
+
+        caixa.style.transform = `translateX(${-idx * 310}px)`
+    }
+
+    setInterval(carrosel, 5000)
+
+})
+
+
+let botoes = document.querySelectorAll('.colors div');
+
+botoes.forEach(function(botao){
+    botao.addEventListener('click', function(){
+        botoes.forEach(function(outroBotao){
+            outroBotao.classList.remove('selecionado')
+        })
+
+        botao.classList.add('selecionado')
+    })
+})
